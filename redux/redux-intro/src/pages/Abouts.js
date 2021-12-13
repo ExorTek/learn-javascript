5b611ca93c5b04c4e72db808cb14b8bc412296b2
ExorTek <exortek979@gmail.com>
2021-12-13 05:00:19 +0300

    Create Abouts.js

---
+++ redux/redux-intro/src/pages/Abouts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Abouts () {
+    return (
+        <div style={{ backgroundColor: 'gray', color: 'white', maxWidth: '4em' }}>
+            Abouts
+        </div>
+
+    );
+}
