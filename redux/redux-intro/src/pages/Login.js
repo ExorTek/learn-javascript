ce8a68639d2d48db85e142efd205e31edf0c0304
ExorTek <exortek979@gmail.com>
2021-12-13 05:00:22 +0300

    Create Login.js

---
+++ redux/redux-intro/src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Login () {
+    return (
+        <div style={{ backgroundColor: 'pink', color: 'white', maxWidth: '4em' }}>
+            Login
+        </div>
+
+    );
+}
