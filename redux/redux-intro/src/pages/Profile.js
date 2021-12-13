0895d3bd0019bd93dc3f3cb1ef72884f1205ccf0
ExorTek <exortek979@gmail.com>
2021-12-13 05:00:24 +0300

    Create Profile.js

---
+++ redux/redux-intro/src/pages/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Profile () {
+    return (
+        <div style={{ backgroundColor: 'red', color: 'white', maxWidth: '4em' }}>
+            Profile
+        </div>
+
+    );
+}
