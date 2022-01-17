1a01d0670866fdbd3c4664b4f28af3b9f8de788d
Mehmet Demirel <54851701+ExorTek@users.noreply.github.com>
2022-01-17 21:26:28 +0300

    Create tokenHelpers.js

---
+++ demo/demotry/fastify-mongoose/helpers/authorization/tokenHelpers.js
@@ -0,0 +1,19 @@
+const sendJwtToClient = (user, reply) => {
+    const token = user.generateAccessToken();
+    const {JWT_COOKIE, NODE_ENV} = process.env;
+    return reply.code(200).header('Authorization', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + parseInt(JWT_COOKIE) * 1000 * 60),
+        secure: NODE_ENV === 'development' ? false : true
+    }).send({
+        success: true,
+        accessToken: token,
+        data: {
+            username: user.username,
+            profileImage: user.profile_image,
+        }
+    });
+};
+
+
+module.exports = {sendJwtToClient};
