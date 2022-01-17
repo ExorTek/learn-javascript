971a673d5aec4488d4533c7a33a6ae8214beb04d
Mehmet Demirel <54851701+ExorTek@users.noreply.github.com>
2022-01-17 21:24:47 +0300

    Update index.js

--- demo/frontend/src/index.js
+++ demo/frontend/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
 import './style/form.css';
+import 'react-image-crop/dist/ReactCrop.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'antd/dist/antd.css';
 import App from './App';
 import {ToastContainer} from 'react-toastify';
 
