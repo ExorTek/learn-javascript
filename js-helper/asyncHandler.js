81068c7b70feee360510683becdac085d4bf700c
Memet <exortek979@gmail.com>
2023-08-09 01:43:06 +0300

    Create asyncHandler.js

---
+++ js-helper/asyncHandler.js
@@ -0,0 +1,12 @@
+/**
+ * Async Handler
+ * @param fn
+ * @return {function(...[*]): Promise<unknown>}
+ * @description This function is used to wrap the async function and catch the error
+ */
+const asyncHandler =
+    fn =>
+        (...args) =>
+            Promise.resolve(fn(...args)).catch(args[2]);
+
+module.exports = asyncHandler;
